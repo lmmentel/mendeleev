4e92492b233f9b5da4c269aae082d0d631a3ae2a
Lukasz Mentel <lmmentel@gmail.com>
2015-10-21 17:45:23 +0200

    introduced **kwargs in the function call to be passed to the seaborn.heatmap

--- mendeleev/plotting.py
+++ mendeleev/plotting.py
@@ -1,8 +1,33 @@
+# -*- coding: utf-8 -*-
+
+#The MIT License (MIT)
+#
+#Copyright (c) 2015 Lukasz Mentel
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 
 import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 from .mendeleev import get_table
 
-def heatmap(prop, style='whitegrid', figsize=(16, 10), cmap='RdBu_r', lw=1, output=None):
+def heatmap(prop, style='whitegrid', figsize=(16, 10), cmap='RdBu_r', lw=1, output=None, **kwargs):
     '''
     Plot a heatmap of the given property
 
@@ -32,7 +57,7 @@ def heatmap(prop, style='whitegrid', figsize=(16, 10), cmap='RdBu_r', lw=1, outp
 
     sns.set(font_scale=1.5, style=style, rc={"figure.figsize": figsize})
     mask = np.asarray(elements_rect.isnull())
-    ax = sns.heatmap(elements_rect, cmap=cmap, mask=mask, linewidths=lw)
+    ax = sns.heatmap(elements_rect, cmap=cmap, mask=mask, linewidths=lw, **kwargs)
     n = len(ax.xaxis.get_ticklabels())
     ax.set_yticklabels(elements_rect.index[::-1], rotation=0)
     ax.set_xticklabels(range(1, n+1))
