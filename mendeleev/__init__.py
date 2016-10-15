fe14f365bc68683967b1d7f62679aa96b661a33b
Lukasz Mentel <lmmentel@gmail.com>
2016-10-16 01:35:50 +0200

    Bump version: 0.2.8 ? 0.2.9

--- mendeleev/__init__.py
+++ mendeleev/__init__.py
@@ -22,7 +22,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 
 from .mendeleev import *
 from .tables import *
