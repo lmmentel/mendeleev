aceb76746ea21e7bad91bd94be9e7a7771ec8204
Lukasz Mentel <lmmentel@gmail.com>
2017-02-10 02:11:08 +0100

    added a function to construct a dataframe with columns necessary for plotting with bokeh

--- mendeleev/utils.py
+++ mendeleev/utils.py
@@ -62,3 +62,45 @@ def get_neutral_data():
     elements = pd.merge(elements, out, on='atomic_number', how='left')
 
     return elements
+
+
+def add_plot_columns(elements):
+    '''
+    Add columns needed for the creating the plots
+
+    Args:
+        elements: pd.DataFrame
+    '''
+
+    mask = elements['group_id'].notnull()
+
+    elements.loc[mask, 'x'] = elements.loc[mask, 'group_id'].astype(int)
+    elements.loc[:, 'y'] = elements.loc[:, 'period'].astype(int)
+
+    elements.loc[mask, 'group_name'] = elements.loc[mask, 'group_id'].astype(int).astype(str)
+    elements.loc[~mask, 'group_name'] = 'f block'
+
+    for period in [6, 7]:
+        mask = (elements['block'] == 'f') & (elements['period'] == period)
+        elements.loc[mask, 'x'] = elements.loc[mask, 'atomic_number'] -\
+                                        elements.loc[mask, 'atomic_number'].min() + 3
+        elements.loc[mask, 'y'] = elements.loc[mask, 'period'] + 2.5
+
+    # additional columns for positioning of the text
+
+    elements.loc[:, 'y_symbol'] = elements['y'] - 0.05
+    elements.loc[:, 'y_anumber'] = elements['y'] - 0.3
+    elements.loc[:, 'y_name'] = elements['y'] + 0.18
+
+    return elements
+
+
+def get_app_data():
+
+    # import mendeleev
+    # ver = mendeleev.__version___
+
+    data = get_neutral_data()
+    data = add_plot_columns(data)
+
+    data.to_pickle('neutral.pkl')
