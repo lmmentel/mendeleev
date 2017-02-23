5b8aee32d359345428ed538d37e2d68e4fff4cfa
Lukasz Mentel <lmmentel@gmail.com>
2017-02-23 21:49:52 +0100

    added Ion class

--- mendeleev/mendeleev.py
+++ mendeleev/mendeleev.py
@@ -374,3 +374,48 @@ def n_eff(n):
     values = {1: 1.0, 2: 2.0, 3: 3.0, 4: 3.7, 5: 4.0, 6: 4.2}
 
     return values.get(n, None)
+
+
+class Ion:
+
+    '''
+
+    # from the element get
+
+    - atomic number
+    - block
+    - group
+    - name
+    - period
+    - series
+    - symbol
+
+    '''
+
+    def __init__(self, label, q):
+
+        self.q = q
+
+        self._set_element_properties(label)
+
+    def _set_element_properties(self, label):
+
+        e = element(label)
+        self.Z = e.atomic_number
+        self.block = e.block
+        self.period = e.period
+        self.mass = e.atomic_weight
+        self.series = e.series
+        self.group = e.group_id
+        self.name = e.name
+        self.symbol = e.symbol
+
+        self.electrons = self.Z - self.q
+
+        self.ie = e.ionenergies[self.q + 1]
+        self.ea = e.ionenergies[self.q]
+
+    def ionic_potential(self):
+        'Calculate the ionic potential'
+
+        return self.q / self.radius
