9a282d919fa477ac43cd706ef81a612c0b020392
Lukasz Mentel <lmmentel@gmail.com>
2017-10-04 14:07:04 +0200

    updated the creation of the elements for import

--- mendeleev/elements.py
+++ mendeleev/elements.py
@@ -1,45 +1,31 @@
+# -*- coding: utf-8 -*-
 
-# inspirations
+#The MIT License (MIT)
+#
+#Copyright (c) 2015 Lukasz Mentel
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 
-# https://stackoverflow.com/questions/1462986/lazy-module-variables-can-it-be-done
-# https://codereview.stackexchange.com/questions/10481/lazy-class-instantiation-in-python
-# https://stackoverflow.com/questions/15226721/python-class-member-lazy-initialization
-# https://stackoverflow.com/questions/7151890/python-lazy-variables-or-delayed-expensive-computation
+from mendeleev import get_all_elements
 
-# https://stackoverflow.com/a/1950986
-# https://stackoverflow.com/questions/28607701/is-it-possible-to-recover-import-after-overwriting-it
+_elements = {x.symbol: x for x in get_all_elements()}
 
-_symbols = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
-            'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
-            'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
-            'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr',
-            'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn',
-            'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
-            'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
-            'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
-            'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
-            'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm',
-            'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
-            'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+globals().update(_elements)
 
-
-#globals().update({k: None for k in _symbols})
-
-import builtins
-
-def custom_import(name, globals=None, locals=None, fromlist=(), level=0):
-
-    print('importing: ', name, fromlist)
-
-    for item_name in fromlist:
-        if item_name not in _symbols:
-            raise ImportError('{} is not an element symbol.'.format(item_name))
-
-    return
-
-
-builtins.__import__ = custom_import
-
-
-
-#__all__ = _symbols
+__all__ = list(_elements.keys())
