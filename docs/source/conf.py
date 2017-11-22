6b5e5b04f758745d33f9c4b6cb9742a3e534cd22
Lukasz Mentel <lmmentel@gmail.com>
2017-11-22 14:44:28 +0100

    small update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ autosummary_generate = True
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.viewcode', 'sphinx.ext.coverage',
-              'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
+              'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
               'sphinxcontrib.bibtex', 'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
