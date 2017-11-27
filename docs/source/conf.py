1b7940717e365d102cced835789f493d025004b0
Lukasz Mentel <lmmentel@gmail.com>
2017-11-27 11:29:51 +0100

    readability improvements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,10 +53,17 @@ autosummary_generate = True
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-              'sphinx.ext.viewcode', 'sphinx.ext.coverage',
-              'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
-              'sphinxcontrib.bibtex', 'nbsphinx']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.coverage',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.mathjax',
+    'sphinxcontrib.bibtex',
+    'nbsphinx'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
