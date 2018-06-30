50e1d117708b6c9d585a9d10ca003b786141ec42
Lukasz Mentel <lmmentel@gmail.com>
2018-06-30 21:37:08 +0200

    added migration script for adding `mendeleev_number` column

---
+++ alembic/versions/07dec3c19f1a_add_mendeleev_number.py
@@ -0,0 +1,27 @@
+"""add mendeleev number
+
+Revision ID: 07dec3c19f1a
+Revises: 4cd9c1a13771
+Create Date: 2018-06-30 21:32:00.940401
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '07dec3c19f1a'
+down_revision = '4cd9c1a13771'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+
+    op.add_column('elements', sa.Column('mendeleev_number', sa.Integer))
+
+
+def downgrade():
+
+    with op.batch_alter_table("elements") as batch_op:
+        batch_op.drop_column('mendeleev_number')
