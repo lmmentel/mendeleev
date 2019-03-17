8159c95d8e24cd591eae0ea06ae288e1bc8aa3fd
Lukasz Mentel <lmmentel@gmail.com>
2019-03-17 19:38:01 +0100

    add migration script for adding dipole polarizability uncetrainty column to db

---
+++ alembic/versions/84f67c0d2733_add_dipole_polarizability_unc_column.py
@@ -0,0 +1,27 @@
+"""add dipole_polarizability_unc column
+
+Revision ID: 84f67c0d2733
+Revises: 07dec3c19f1a
+Create Date: 2019-03-17 19:20:17.426746
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '84f67c0d2733'
+down_revision = '07dec3c19f1a'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+
+    op.add_column('elements', sa.Column('dipole_polarizability_unc', sa.Float))
+
+
+def downgrade():
+
+    with op.batch_alter_table('elements') as batch_op:
+        batch_op.drop_column('dipole_polarizability_unc')
