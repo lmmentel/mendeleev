ee5edd254ccca75d3619b2a98006bdcf3c706659
Lukasz Mentel <lmmentel@gmail.com>
2019-03-17 17:51:22 +0100

    add more test coverage for getting elements and printing

--- tests/test_element.py
+++ tests/test_element.py
@@ -1,7 +1,12 @@
 
 
 import pytest
-from mendeleev import get_session, Element, element
+from mendeleev import get_session, Element, element, get_all_elements
+
+
+ALL_ELEMENTS = {x.symbol: x for x in get_all_elements()}
+SYMBOLS = list(ALL_ELEMENTS.keys())
+NAMES = [x.name for x in ALL_ELEMENTS.values()]
 
 
 @pytest.fixture
@@ -20,3 +25,55 @@ def test_element():
 
     si = element('Si')
     assert si.name == 'Silicon'
+
+
+@pytest.mark.parametrize("atomic_number", list(range(1, 119)))
+def test_elements_get_by_atomic_number(atomic_number):
+
+    e = element(atomic_number)
+    assert e.atomic_number == atomic_number
+
+
+@pytest.mark.parametrize("symbol", SYMBOLS)
+def test_elements_get_by_symbol(symbol):
+
+    e = element(symbol)
+    assert e.symbol == symbol
+
+
+@pytest.mark.parametrize("name", NAMES)
+def test_elements_get_by_name(name):
+
+    e = element(name)
+    assert e.name == name
+
+
+@pytest.mark.parametrize("symbol", SYMBOLS)
+def test_elements_str(symbol):
+
+    e = element(symbol)
+    str(e)
+
+
+@pytest.mark.parametrize("symbol", SYMBOLS)
+def test_elements_repr(symbol):
+
+    e = element(symbol)
+    repr(e)
+
+
+@pytest.mark.parametrize("symbol", SYMBOLS)
+def test_isotopes_str(symbol):
+
+    e = element(symbol)
+    for i in e.isotopes:
+        str(e)
+        repr(e)
+
+
+@pytest.mark.parametrize("symbol", SYMBOLS)
+def test_isotopes_repr(symbol):
+
+    e = element(symbol)
+    for i in e.isotopes:
+        repr(e)
