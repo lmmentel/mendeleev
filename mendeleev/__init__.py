45928c8e8abb13bdd2f4af3bea90ab7062ec9c3a
Lukasz Mentel <lmmentel@gmail.com>
2019-08-26 00:13:38 +0200

    Bump version: 0.4.5 → 0.5.0

--- mendeleev/__init__.py
+++ mendeleev/__init__.py
@@ -22,7 +22,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-__version__ = '0.4.5'
+__version__ = '0.5.0'
 
 from .mendeleev import *
 from .tables import *
