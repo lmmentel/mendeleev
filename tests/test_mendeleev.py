a3182df049e4d707eb367fde3eea860d004960f4
Lukasz Mentel <lmmentel@gmail.com>
2019-08-26 11:29:51 +0200

    add tests to get_table for all tables

---
+++ tests/test_mendeleev.py
@@ -0,0 +1,21 @@
+
+from mendeleev import get_table
+import pytest
+
+
+expected = [
+    ('elements', 118),
+    ('isotopes', 406),
+    ('ionicradii', 492),
+    ('ionizationenergies', 5837),
+    ('groups', 18),
+    ('series', 10),
+    ('oxidationstates', 231),
+]
+
+
+@pytest.mark.parametrize('table_name,nrows', expected)
+def test_get_table(table_name, nrows):
+
+    table = get_table(table_name)
+    assert table.shape[0] == nrows
