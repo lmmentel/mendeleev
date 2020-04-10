1f9b21d98d38c5914f1e2e6a5e1f6fb74a7121e1
Lukasz Mentel <lmmentel@gmail.com>
2020-04-10 22:03:51 +0200

    first Ion class implementation

--- mendeleev/ion.py
+++ mendeleev/ion.py
@@ -1,59 +1,113 @@
 
+from statistics import mean
 from mendeleev import element
 
 
 class Ion:
+    """
+    A class for dealing with atomic ions
+    """
 
-    '''
+    __element_attributes__ = ['atomic_number', 'block', 'group', 'series', 'period', 'mass', 'symbol']
+    
+    def __init__(self, label, q=1):
 
-    # from the element get
-
-    - atomic number
-    - block
-    - group
-    - name
-    - period
-    - series
-    - symbol
-
-    '''
-
-    def __init__(self, label, q):
-
+        self._element = element(label)
         self.q = q
 
-        self._set_element_properties(label)
+    @property
+    def q(self):
+        return self._q
 
-    def _set_element_properties(self, label):
-
-        e = element(label)
-        self.Z = e.atomic_number
-        self.block = e.block
-        self.period = e.period
-        self.mass = e.atomic_weight
-        self.series = e.series
-        self.group = e.group_id
-        self.name = e.name
-        self.symbol = e.symbol
-
-        self.electrons = self.Z - self.q
-
-        self.ie = e.ionenergies[self.q + 1]
-        self.ea = e.ionenergies[self.q]
+    @q.setter
+    def q(self, value):
+        if value == 0:
+            raise ValueError('expecting change other than 0, got {}'.format(value))
+        elif value > self.Z:
+                raise ValueError('ionic charge ({}) cannot be larger than atomic number ({})'.format(value, self.Z))
+        else:
+            self._q = int(value)
 
     @property
+    def Z(self):
+        return self._element.atomic_number
+            
+    @property
+    def charge(self):
+        return self._q
+        
+    @property
     def electrons(self):
-        return self._electrons
-
-    @electrons.setter
-    def electrons(self):
-
-        if abs(self.q) > self.Z:
-            self._electrons = self.Z - self.q
-
+        return self.Z - self.q
 
     @property
-    def ionic_potential(self):
-        'Calculate the ionic potential'
+    def name(self):
+        sign = "+" if self.charge > 0 else "-"
+        return "{} {}{} ion".format(self._element.name, self.charge, sign)
 
-        return self.q / self.radius
+    @property
+    def ie(self):
+        return self._element.ionenergies[self.q + 1]
+    
+    @property
+    def ea(self):
+        return self._element.ionenergies[self.q]
+
+    @property
+    def radius(self):
+        return [r for r in self._element.ionic_radii if r.charge == self.charge]
+
+    def unicode_ion_symbol(self):
+        """
+        Return a unicode string symbol of the ion
+        """
+
+        superscripts = {
+            "+": u"\u207A",
+            "-": u"\u207B",
+            "0": u"\u2070",
+            "1": u"\u00B9",    
+            "2": u"\u00B2",
+            "3": u"\u00B3",
+            "4": u"\u2074",
+            "5": u"\u2075",
+            "6": u"\u2076",
+            "7": u"\u2077",
+            "8": u"\u2078",
+            "9": u"\u2079",
+        }
+        table = str.maketrans(superscripts)
+        template = "+" if self.charge > 0 else "-"
+
+        if abs(self.charge) != 1:
+            template = str(abs(self.charge)) + template
+
+        return self.symbol + template.translate(table)
+
+    def ionic_potential(self, radius_most_reliable: bool = True) -> float:
+        """
+        Calculate the ionic potential
+        
+        Args:
+            radius_most_reliable : flag to use the most reliable ionic radius,
+                default is `True`
+        
+        Returns:
+            ionic potential
+        """
+
+        if radius_most_reliable:
+            radius = mean([r.ionic_radius for r in self.radius if r.most_reliable])
+        else:
+            radius = mean([r.ionic_radius for r in self.radius])
+        return self.q / radius
+
+    def __getattr__(self, name):
+        
+        if name in Ion.__element_attributes__:
+            return getattr(self._element, name)
+        else:
+            raise AttributeError("'{}' is not na attribute of '{}'".format(name, self.__class__.__name__))
+
+    def __repr__(self):
+        return self.unicode_ion_symbol()
--- tests/test_ion.py
+++ tests/test_ion.py
@@ -1,12 +1,72 @@
 
 import pytest
-from mendeleev import Ion
+from mendeleev import element
+from mendeleev.ion import Ion
 
 
-def test_H():
+@pytest.mark.parametrize("atomic_number", list(range(1, 119)))
+def test_inherited_attrs(atomic_number):
+    """
+    Check if properties inherited from Element are properly passed on 
+    """
 
-    hplus = Ion('He', +1)
+    elm = element(atomic_number)
+    ion = Ion(atomic_number)
 
-    assert hplus.name == 'Hydrogen'
-    assert hplus.electrons == 0
-    assert hplus.charge == 1
+    assert ion.charge == ion.q
+    assert ion.Z == ion.atomic_number == elm.atomic_number
+    assert ion.electrons == ion.Z - ion.charge
+
+    for attr in Ion.__element_attributes__:
+        if attr != 'group':
+            assert getattr(elm, attr) == getattr(ion, attr)
+
+
+@pytest.fixture
+def he():
+    return Ion('He', 1)
+
+
+@pytest.fixture
+def fe2():
+    return Ion('Fe', 2)
+
+
+def test_basic_he_properties(he):
+    assert he.symbol == 'He'
+    assert he.name == 'Helium 1+ ion'
+    assert he.charge == 1
+    assert he.electrons == 1
+
+
+def test_zero_charge():
+    with pytest.raises(ValueError):
+        Ion('He', 0)
+
+
+def test_charge_too_large():
+    with pytest.raises(ValueError):
+        Ion('He', 3)
+
+
+def test_forbidden_attribute(he):
+    with pytest.raises(AttributeError):
+        getattr(he, 'electronegativity')
+
+
+def test_ionic_potential(fe2):
+    assert pytest.approx(fe2.ionic_potential(), 2 / 78.0)
+    assert pytest.approx(fe2.ionic_potential(radius_most_reliable=False), 2 / 71.6) 
+
+
+def test_ea(fe2):
+    assert pytest.approx(fe2.ea, 16.1992)
+
+
+def test_ie(fe2):    
+    assert pytest.approx(fe2.ie, 30.651)
+
+
+def test_repr(fe2):
+    assert repr(fe2) == 'Fe²⁺'
+    assert str(fe2) == 'Fe²⁺'
