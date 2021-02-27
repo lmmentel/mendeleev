6166a9701db170371d9e26cacbdbe3af2693560e
Lukasz Mentel <lmmentel@gmail.com>
2021-02-27 16:34:26 +0100

    update number of ionic radii

--- tests/test_mendeleev.py
+++ tests/test_mendeleev.py
@@ -1,20 +1,19 @@
-
 from mendeleev import get_table
 import pytest
 
 
 expected = [
-    ('elements', 118),
-    ('isotopes', 406),
-    ('ionicradii', 492),
-    ('ionizationenergies', 5837),
-    ('groups', 18),
-    ('series', 10),
-    ('oxidationstates', 231),
+    ("elements", 118),
+    ("isotopes", 406),
+    ("ionicradii", 507),
+    ("ionizationenergies", 5837),
+    ("groups", 18),
+    ("series", 10),
+    ("oxidationstates", 231),
 ]
 
 
-@pytest.mark.parametrize('table_name,nrows', expected)
+@pytest.mark.parametrize("table_name,nrows", expected)
 def test_get_table(table_name, nrows):
 
     table = get_table(table_name)
