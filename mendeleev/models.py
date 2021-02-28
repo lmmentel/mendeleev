6c021a003019decd8687409e7682de94c1d6fd6e
Lukasz Mentel <lmmentel@gmail.com>
2021-02-28 17:41:48 +0100

    reformat

--- mendeleev/models.py
+++ mendeleev/models.py
@@ -1,32 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# The MIT License (MIT)
-#
-# Copyright (c) 2015 Lukasz Mentel
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 """module specifying the database models"""
 
+from typing import Any, List, Tuple
 from operator import attrgetter
-import numpy as np
 
+import numpy as np
 from sqlalchemy import Column, Boolean, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship, reconstructor
 from sqlalchemy.ext.associationproxy import association_proxy
@@ -43,8 +22,7 @@ from .electronegativity import (
     mulliken,
     sanderson,
 )
-
-# from .queries import estimate_from_group
+from .db import get_session
 from .econf import ElectronicConfiguration, get_l, ORBITALS
 
 
@@ -734,10 +712,6 @@ class Element(Base):
         )
 
 
-from typing import Any, List, Tuple
-from .db import get_session
-
-
 def fetch_attrs_for_group(attrs: List[str], group: int = 18) -> Tuple[List[Any]]:
     """
     A convenience function for getting a specified attribute for all
@@ -763,11 +737,6 @@ def fetch_attrs_for_group(attrs: List[str], group: int = 18) -> Tuple[List[Any]]
     results = tuple([getattr(member, attr) for member in members] for attr in attrs)
     session.close()
     return results
-
-
-import numpy as np
-
-# from .selectors import fetch_attrs_for_group
 
 
 def estimate_from_group(
