10c823e0814d41022081d0a18569e947f94613dc
Lukasz Mentel <lmmentel@gmail.com>
2021-02-28 17:25:42 +0100

    move all the data fetching functions to fetch module and refactor some of them

--- mendeleev/fetch.py
+++ mendeleev/fetch.py
@@ -1,11 +1,13 @@
+from typing import List, Union
+
 import pandas as pd
 from sqlalchemy.dialects import sqlite
 
-from mendeleev import element, get_table
+from mendeleev import element
 from mendeleev import __version__ as version
 
 from .db import get_engine, get_session
-from .tables import IonizationEnergy
+from .tables import Element, IonizationEnergy
 
 
 def get_zeff(an, method="slater") -> float:
@@ -15,14 +17,55 @@ def get_zeff(an, method="slater") -> float:
     return e.zeff(method=method)
 
 
-def get_neutral_data() -> pd.DataFrame:
+def fetch_table(table: str, **kwargs) -> pd.DataFrame:
+    """
+    Return a table from the database as `pandas <http://pandas.pydata.org/>`_
+    `DataFrame <http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html>`_
+
+    Args:
+      table: Name of the table from the database
+      kwargs: A dictionary of keyword arguments to pass to the `pandas.read_qsl`
+
+    Returns:
+      df: pandas.DataFrame
+        Pandas DataFrame with the contents of the table
+
+    Example:
+        >>> from mendeleev.fetch import fetch_table
+        >>> df = fetch_table('elements')
+        >>> type(df)
+        pandas.core.frame.DataFrame
+
+    """
+
+    tables = {
+        "elements",
+        "groups",
+        "isotopes",
+        "ionicradii",
+        "ionizationenergies",
+        "oxidationstates",
+        "screeningconstants",
+        "series",
+    }
+
+    if table not in tables:
+        raise ValueError(
+            f"Table '{table}' not found, available tables are: {', '.join(sorted(tables))}"
+        )
+
+    engine = get_engine()
+    return pd.read_sql(table, engine, **kwargs)
+
+
+def fetch_neutral_data() -> pd.DataFrame:
     """
     Get extensive set of data from multiple database tables as pandas.DataFrame
     """
 
-    elements = get_table("elements")
-    series = get_table("series")
-    groups = get_table("groups")
+    elements = fetch_table("elements")
+    series = fetch_table("series")
+    groups = fetch_table("groups")
 
     elements = pd.merge(
         elements,
@@ -91,6 +134,86 @@ def get_neutral_data() -> pd.DataFrame:
     return elements
 
 
+def fetch_ionic_radii(radius: str = "ionic_radius") -> pd.DataFrame:
+    """
+    Fetch a pandas DataFrame with ionic radii for all the elements.
+
+    Args:
+      radius: The radius to be returned either `ionic_radius` or `crystal_radius`
+
+    Returns:
+      df: DataFrame
+        Pandas DataFrame with atomic numbers, symbols and ionic radii for all
+        coordinations
+    """
+
+    if radius not in ["ionic_radius", "crystal_radius"]:
+        raise ValueError('wrong values, should be "ionic_radius" or "crystal_radius"')
+
+    ir = fetch_table("ionicradii")
+    ir = ir[ir.spin != "HS"]
+    # create new temporary pseudo multiindex
+    ir["idx"] = ir.atomic_number.astype(str) + "(" + ir.charge.astype(str) + ")"
+    df = ir.pivot(index="idx", columns="coordination", values="ionic_radius")
+    # get back the atomic_number and charge columns from idx
+    df["atomic_number"] = df.index.str.extract(r"^(\d+)")
+    df["charge"] = df.index.str.extract(r"^\d+\((-?\d+)\)")
+    df.reset_index(inplace=True)
+    df.drop("idx", axis=1, inplace=True)
+
+    return df
+
+
+def fetch_ionization_energies(degree: Union[List[int], int] = 1) -> pd.DataFrame:
+    """
+    Fetch a pandas DataFrame with ionization energies for all elements indexed by atomic_number
+
+    Args:
+        degree: Degree of ionization, either as int or a list of ints. If a list is
+            passed then the output will contain ionization energies corresponding
+            to particalr degrees in columns.
+
+    Returns:
+        df: pandas DataFrame with ionization energies, indexed by `index_by`
+    """
+
+    # validate degree
+    if isinstance(degree, (list, tuple, set)):
+        if not all(isinstance(d, int) for d in degree) or any(d <= 0 for d in degree):
+            raise ValueError("degree should be a list of positive ints")
+    elif isinstance(degree, int):
+        if degree <= 0:
+            raise ValueError(f"degree should be positive")
+        degree = [degree]
+    else:
+        raise ValueError(
+            f"degree should be either a positive int or a collection of positive ints, got: {degree}"
+        )
+
+    session = get_session()
+    engine = get_engine()
+
+    query = session.query(Element.atomic_number).order_by("atomic_number")
+    df = pd.read_sql_query(query.statement.compile(dialect=sqlite.dialect()), engine)
+
+    for d in degree:
+        query = session.query(IonizationEnergy).filter(IonizationEnergy.degree == d)
+        energies = pd.read_sql_query(
+            query.statement.compile(dialect=sqlite.dialect()), engine
+        )
+
+        df = pd.merge(
+            df,
+            energies.loc[:, ["atomic_number", "energy"]].rename(
+                columns={"energy": "IE{0:d}".format(d)}
+            ),
+            on="atomic_number",
+            how="left",
+        )
+
+    return df.set_index("atomic_number")
+
+
 def add_plot_columns(elements: pd.DataFrame) -> pd.DataFrame:
     """
     Add columns needed for the creating the plots
@@ -130,7 +253,7 @@ def add_plot_columns(elements: pd.DataFrame) -> pd.DataFrame:
 def get_app_data():
     "write a file with the neutral data"
 
-    data = get_neutral_data()
+    data = fetch_neutral_data()
     data = add_plot_columns(data)
     fname = "neutral_{0:s}.pkl".format(version)
     data.to_pickle(fname)
---
+++ tests/test_fetch.py
@@ -0,0 +1,51 @@
+import pytest
+import pandas as pd
+from mendeleev.fetch import (
+    fetch_ionic_radii,
+    fetch_ionization_energies,
+    fetch_neutral_data,
+    fetch_table,
+)
+
+
+tables = [
+    ("elements", 118),
+    ("isotopes", 406),
+    ("ionicradii", 507),
+    ("ionizationenergies", 5837),
+    ("groups", 18),
+    ("series", 10),
+    ("oxidationstates", 231),
+]
+
+
+@pytest.mark.parametrize("table,nrows", tables)
+def test_fetch_table(table, nrows):
+
+    df = fetch_table(table)
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape[0] == nrows
+
+
+def test_fetch_neutral_data():
+
+    df = fetch_neutral_data()
+    assert isinstance(df, pd.DataFrame)
+
+
+@pytest.mark.parametrize("radius", ("ionic_radius", "crystal_radius"))
+def test_fetch_ionic_radii(radius):
+
+    df = fetch_ionic_radii(radius)
+    assert isinstance(df, pd.DataFrame)
+
+
+@pytest.mark.parametrize(
+    "degree,cols",
+    ((1, ["IE1"]), ([1, 2], ["IE1", "IE2"]), ([1, 2, 6], ["IE1", "IE2", "IE6"])),
+)
+def test_fetch_ionization_energies(degree, cols):
+
+    df = fetch_ionization_energies(degree)
+    assert isinstance(df, pd.DataFrame)
+    assert all(col in df.columns for col in cols)
