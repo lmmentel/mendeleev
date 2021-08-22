a9a46c3c4d52cff5bd3a56f4fdd471c18efc583e
Lukasz Mentel <lmmentel@gmail.com>
2021-08-22 19:46:39 +0200

    reanme arguments from attr to coord and update colormap_column

--- mendeleev/vis/utils.py
+++ mendeleev/vis/utils.py
@@ -8,9 +8,9 @@ from mendeleev.fetch import fetch_table
 
 def add_tile_coordinates(
     df: pd.DataFrame,
-    x_attr: str = "group_id",
-    y_attr: str = "period",
-    show_fblock: bool = True,
+    x_coord: str = "group_id",
+    y_coord: str = "period",
+    include_f_block: bool = True,
     wide_layout: bool = False,
 ) -> pd.DataFrame:
     """
@@ -18,24 +18,24 @@ def add_tile_coordinates(
 
     Args:
         df: dataframe
-        x_attr: attribute to use as x coordinate
-        y_attr: attribute to use as y coordinate
+        x_coord: attribute to use as x coordinate
+        y_coord: attribute to use as y coordinate
         wide_layout : Show the long version of the periodic table with the f block between
             the s and d blocks
-        show_fblock : Show the elements from the f block
+        include_f_block : Show the elements from the f block
     """
 
     elements = df.copy().sort_values("atomic_number")
 
     # calculate x and y coordinates of the main group/row elements
-    elements.loc[elements[x_attr].notnull(), "x"] = elements.loc[
-        elements[x_attr].notnull(), x_attr
+    elements.loc[elements[x_coord].notnull(), "x"] = elements.loc[
+        elements[x_coord].notnull(), x_coord
     ].astype(int)
-    elements.loc[elements[y_attr].notnull(), "y"] = elements.loc[
-        elements[y_attr].notnull(), y_attr
+    elements.loc[elements[y_coord].notnull(), "y"] = elements.loc[
+        elements[y_coord].notnull(), y_coord
     ].astype(int)
 
-    if show_fblock:
+    if include_f_block:
         f_mask = elements["block"] == "f"
         elements.loc[f_mask, "x"] = (
             elements[f_mask]
@@ -61,18 +61,18 @@ def add_tile_coordinates(
 
 
 def create_vis_dataframe(
-    x_attr: str = "group_id",
-    y_attr: str = "period",
-    show_f_block: bool = True,
+    x_coord: str = "group_id",
+    y_coord: str = "period",
+    include_f_block: bool = True,
     wide_layout: bool = False,
 ):
     """
     Base DataFrame for visualizations
 
     Args:
-        x_attr: attribute to use as x coordinate
-        y_attr: attribute to use as y coordinate
-        show_f_block : show the elements from the f block
+        x_coord: attribute to use as x coordinate
+        y_coord: attribute to use as y coordinate
+        include_f_block : show the elements from the f block
         wide_layout : show the long version of the periodic table with the f block between
             the s and d blocks
 
@@ -88,9 +88,9 @@ def create_vis_dataframe(
     ).sort_values(by="atomic_number")
     return add_tile_coordinates(
         df,
-        x_attr=x_attr,
-        y_attr=y_attr,
-        show_fblock=show_f_block,
+        x_coord=x_coord,
+        y_coord=y_coord,
+        include_f_block=include_f_block,
         wide_layout=wide_layout,
     )
 
@@ -112,10 +112,13 @@ def colormap_column(
     colormap = plt.get_cmap(cmap)
     cnorm = colors.Normalize(vmin=elements[column].min(), vmax=elements[column].max())
     scalarmap = cmx.ScalarMappable(norm=cnorm, cmap=colormap)
-    out = pd.DataFrame(index=elements.index)
-    mask = elements[column].isnull()
-    rgba = scalarmap.to_rgba(elements[column])
-    out.loc[:, "cmap"] = [colors.rgb2hex(row) for row in rgba]
-    out.loc[mask, "cmap"] = missing
 
-    return out
+    rgba = scalarmap.to_rgba(elements[column])
+
+    colored = pd.Series(
+        index=elements.index, data=[colors.rgb2hex(row) for row in rgba]
+    )
+    mask = elements[column].isnull()
+    colored.loc[mask] = missing
+
+    return colored
