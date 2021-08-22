f2b1525c43dce097997f278bab147d5ff698c2ab
Lukasz Mentel <lmmentel@gmail.com>
2021-08-22 19:48:24 +0200

    enable color by attribute feature

--- mendeleev/vis/plotly.py
+++ mendeleev/vis/plotly.py
@@ -3,21 +3,23 @@ import plotly.graph_objects as go
 from plotly.graph_objs.layout import Shape, Annotation
 from pandas.api.types import is_float_dtype
 
+from .utils import colormap_column
+
 
 def create_tile(
-    row: pd.Series, x_offset: float = 0.45, y_offset: float = 0.45
+    element: pd.Series, color: str, x_offset: float = 0.45, y_offset: float = 0.45
 ) -> Shape:
     """
     Create tile shape
     """
     return Shape(
         type="rect",
-        x0=row["x"] - x_offset,
-        y0=row["y"] - y_offset,
-        x1=row["x"] + x_offset,
-        y1=row["y"] + y_offset,
-        line=dict(color=row["color"]),
-        fillcolor=row["color"],
+        x0=element["x"] - x_offset,
+        y0=element["y"] - y_offset,
+        x1=element["x"] + x_offset,
+        y1=element["y"] + y_offset,
+        line=dict(color=element[color]),
+        fillcolor=element[color],
         opacity=0.8,
     )
 
@@ -48,11 +50,14 @@ def create_annotation(
 def periodic_table_plotly(
     elements: pd.DataFrame,
     attribute: str = "atomic_weight",
-    height: int = 800,
-    width: int = 1200,
+    cmap: str = "RdBu_r",
+    colorby: str = "color",
     decimals: int = 3,
+    height: int = 800,
+    missing: str = "#ffffff",
     title: str = "Periodic Table",
     wide_layout: bool = False,
+    width: int = 1200,
 ) -> go.Figure:
     """
     Create a periodic table visualization with plotly.Figure
@@ -60,16 +65,24 @@ def periodic_table_plotly(
     Args:
         elements : Pandas DataFrame with the data about elements
         attribute : Name of the attribute to be displayed
-        title : Title to appear above the periodic table
-        width : Width of the figure in pixels
-        height : Height of the figure in pixels
+        cmap : Colormap to use, see matplotlib colormaps
+        colorby : Name of the column containig the colors
         decimals : Number of decimals to be displayed in the bottom row of each cell
+        height : Height of the figure in pixels
+        missing : Hex code of the color to be used for the missing values
+        title : Title to appear above the periodic table
         wide_layout: wide layout variant of the periodic table
+        width : Width of the figure in pixels
     """
     fig = go.Figure()
 
+    if colorby == "attribute":
+        colored = colormap_column(elements, attribute, cmap=cmap, missing=missing)
+        elements.loc[:, "attribute_color"] = colored
+        colorby = "attribute_color"
+
     # tiles
-    tiles = [create_tile(row) for _, row in elements.iterrows()]
+    tiles = [create_tile(row, color=colorby) for _, row in elements.iterrows()]
     fig.layout["shapes"] += tuple(tiles)
 
     # symbols
