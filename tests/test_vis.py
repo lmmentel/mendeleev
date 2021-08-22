ed9010b2cdb99a5ac59a59a814c274b4cfa8166f
Lukasz Mentel <lmmentel@gmail.com>
2021-08-22 19:44:15 +0200

    add test for periodic_table visualizations

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -1,11 +1,13 @@
+import pytest
 from mendeleev.fetch import fetch_table
-from plotly.graph_objects import Figure
-from bokeh.plotting import figure
+from plotly.graph_objects import Figure as PlotlyFigure
+from bokeh.plotting import Figure as BokehFigure
 from mendeleev.vis import create_vis_dataframe, add_tile_coordinates
 from mendeleev.vis import (
     periodic_table_plotly,
     periodic_table_bokeh,
     heatmap,
+    periodic_table,
 )
 
 
@@ -23,7 +25,7 @@ def test_periodic_table_plotly():
     elements = create_vis_dataframe()
     fig = periodic_table_plotly(elements)
 
-    assert isinstance(fig, Figure)
+    assert isinstance(fig, PlotlyFigure)
 
 
 def test_periodic_table_bokeh():
@@ -31,7 +33,7 @@ def test_periodic_table_bokeh():
     elements = create_vis_dataframe()
     fig = periodic_table_bokeh(elements)
 
-    assert isinstance(fig, figure)
+    assert isinstance(fig, BokehFigure)
 
 
 def test_periodic_table_seaborn():
@@ -39,4 +41,13 @@ def test_periodic_table_seaborn():
     elements = create_vis_dataframe()
     fig = heatmap(elements, "c6")
 
-    assert isinstance(fig, Figure)
+
+@pytest.mark.parametrize("attribute", ["atomic_weight", "atomic_radius"])
+@pytest.mark.parametrize("colorby", ["color", "jmol_color", "attribute"])
+@pytest.mark.parametrize("wide_layout", [False, True])
+@pytest.mark.parametrize("backend", ["plotly", "bokeh"])
+def test_periodic_table(attribute, colorby, wide_layout, backend):
+
+    fig = periodic_table(
+        attribute=attribute, colorby=colorby, wide_layout=wide_layout, backend=backend
+    )
