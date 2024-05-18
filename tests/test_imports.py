8158bf6e7fb721fb133089ab8dee94ac32727110
Lukasz Mentel <lmmentel@gmail.com>
2024-05-18 13:51:18 +0200

    add import tests

---
+++ tests/test_imports.py
@@ -0,0 +1,39 @@
+import pytest
+
+
+def test_from_import():
+    """Test that the main imports work"""
+    from mendeleev import fetch
+
+    assert fetch is not None
+    from mendeleev import vis
+
+    assert vis is not None
+    from mendeleev import db
+
+    assert db is not None
+    from mendeleev import ion
+
+    assert ion is not None
+    from mendeleev import models
+
+    assert models is not None
+    from mendeleev import mendeleev
+
+    assert mendeleev is not None
+
+
+def test_direct_import():
+    """Test that the main imports work"""
+    import mendeleev.db  # noqa: F401
+    import mendeleev.fetch  # noqa: F401
+    import mendeleev.vis  # noqa: F401
+    import mendeleev.ion  # noqa: F401
+    import mendeleev.mendeleev  # noqa: F401
+    import mendeleev.models  # noqa: F401
+
+
+def test_missing_import():
+    """Test that the import of a non-existing module raises ImportError"""
+    with pytest.raises(ImportError):
+        from mendeleev import nonexisting  # noqa: F401
