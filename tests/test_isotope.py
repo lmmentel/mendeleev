88510cc091de7287b7c897bfac764433443c136e
Lukasz Mentel <lmmentel@users.noreply.github.com>
2024-06-01 20:59:28 +0200

    Update Isotope.half_life_unit values (#160)
    * add test for isotop half_life_unit values
    
    * updated to isotopes.half_life_units and annotations

--- tests/test_isotope.py
+++ tests/test_isotope.py
@@ -1,4 +1,6 @@
 from mendeleev import isotope
+from mendeleev.db import get_session
+from mendeleev.models import Isotope
 
 
 def test_get_isotope():
@@ -10,3 +12,34 @@ def test_get_isotope():
     result = isotope("H", 3)
     assert result.atomic_number == 1
     assert result.mass_number == 3
+
+
+def test_isotopes_half_life_units():
+    reference_units = (
+        None,
+        "asec",
+        "day",
+        "Eyear",
+        "Gyear",
+        "hour",
+        "kyear",
+        "minute",
+        "msec",
+        "Myear",
+        "nsec",
+        "psec",
+        "Pyear",
+        "sec",
+        "Tyear",
+        "usec",
+        "year",
+        "ysec",
+        "Yyear",
+        "zsec",
+        "Zyear",
+    )
+
+    sess = get_session()
+    units = sess.query(Isotope.half_life_unit).distinct().all()
+    units = [u[0] for u in units]
+    assert set(units) == set(reference_units)
