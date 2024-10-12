1aabe2ab3cf1e37016f4b33747bfb449843cff90
Lukasz Mentel <lmmentel@gmail.com>
2024-10-13 00:53:20 +0200

    docs

--- mendeleev/interfaces/nist_asd.py
+++ mendeleev/interfaces/nist_asd.py
@@ -1,7 +1,11 @@
 """Interface to the NIST Atomic Spectra Database (ASD)"""
 
-from pydantic import BaseModel
+import io
 from enum import Enum
+
+import pandas as pd
+import requests
+from pydantic import BaseModel
 
 
 class Units(Enum):
@@ -21,8 +25,8 @@ class Format(Enum):
 
 class Query(BaseModel):
     spectra: str
-    units: int = Units.eV
-    format: int = Format.CSV
+    units: int = Units.eV.value
+    format: int = Format.CSV.value
     order: int = 0
     at_num_out: str = "on"
     sp_name_out: str = "on"
@@ -40,6 +44,31 @@ class Query(BaseModel):
     base_url: str = "https://physics.nist.gov/cgi-bin/ASD/ie.pl"
 
     def request(self):
-        return f"{self.base_url}?" + "&".join(
-            [f"{k}={v}" for k, v in self.dict().items()]
-        )
+        params = {k: v for k, v in self.model_dump().items() if k not in {"base_url"}}
+
+        return f"{self.base_url}?" + "&".join([f"{k}={v}" for k, v in params.items()])
+
+    def fetch_data(self):
+        response = requests.get(self.request())
+        response.raise_for_status()
+
+        # find the Notes section if exists
+        try:
+            index = response.text.index("Notes")
+        except ValueError:
+            index = len(response.text)
+        csv_data = response.text[:index]
+        # csv_notes = response.text[index:]
+        csv_file = io.StringIO(csv_data)
+        return pd.read_csv(csv_file)
+
+
+def clean(df):
+    "Clean the data frame from NIST ASD"
+    df = df.dropna(axis="columns")
+    df = df.apply(lambda x: x.str.strip("="))
+    df = df.apply(lambda x: x.str.strip('"'))
+    df["At. num"] = pd.to_numeric(df["At. num"])
+    df["Ionization Energy (eV)"] = pd.to_numeric(df["Ionization Energy (eV)"])
+    df["Uncertainty (eV)"] = pd.to_numeric(df["Uncertainty (eV)"])
+    return df
